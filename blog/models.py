6ab129696d52eb0c184509721c9149a47a419f0d
rawteech <rotichbil636@gmail.com>
2017-04-02 23:09:21 +0300

    Added templating to the views

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 
 class PublishedManager(models.Manager):
@@ -35,3 +36,9 @@ class Post(models.Model):
 	def __str__(self):
 		return self.title
 
+	def get_absolute_url(self):
+		return reverse('blog:post_detail',
+					   args=[self.publish.year,
+					   		 self.publish.strftime('%m'),
+					   		 self.publish.strftime('%d'),
+					   		 self.slug])
