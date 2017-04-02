945834de78157a468e9dc17f3af6181c211c23eb
rawteech <rotichbil636@gmail.com>
2017-04-02 22:28:04 +0300

    Created View and Urls

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,11 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+	def get_queryset(self):
+		return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
 	STATUS_CHOICES = (
 		('draft', 'Draft'),
@@ -20,6 +25,8 @@ class Post(models.Model):
 	status = models.CharField(max_length=10,
 							  choices=STATUS_CHOICES,
 							  default='draft')
+	objects = models.Manager()
+	published = PublishedManager()
 
 
 	class Meta:
@@ -27,3 +34,4 @@ class Post(models.Model):
 
 	def __str__(self):
 		return self.title
+
---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from blog import views
+
+
+urlpatterns = [
+	url(r'^$', views.post_list, name='post_list'),
+	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
+		r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
+]
