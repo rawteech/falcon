c41bbce662acbb28d96256887bb20b330c76534d
rawteech <rotichbil636@gmail.com>
2017-04-04 03:52:04 +0300

    Added views and urls for mail sending

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
 	url(r'^$', views.PostListView.as_view(), name='post_list'),
 	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
 		r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
+	url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.mail import send_mail
 from django.views.generic import ListView
 
 from blog.models import Post
 from blog.forms import EmailPostForm
+
+from decouple import config
 
 
 # def post_list(request):
@@ -39,17 +42,24 @@ def post_detail(request, year, month, day, post):
 def post_share(request, post_id):
 	# Retrieving post by id
 	post = get_object_or_404(Post, id=post_id, status='published')
+	sent = False
 
 	if request.method == 'POST':
 		# Meaning form was submitted
-		form = EmailPostForm(request.post)
+		form = EmailPostForm(request.POST)
 		if form.is_valid():
 			# Validation passed
 			cd = form.cleaned_data
+			
+			post_url = request.build_absolute_uri(post.get_absolute_url())
+			subject = '{} ({}) recommends you reading "{}"'.format(cd['name'], cd['email'], post.title)
+			message = 'Read "{}" at {}\n\n{}\'s comments: {}'.format(post.title, post_url, cd['name'], cd['comments'])
+			send_mail(subject, message, config('EMAIL_HOST_USER'), [cd['to']])
+			sent = True
 	else:
 		form = EmailPostForm()
 
-	context = {'post': post, 'form': form}
+	context = {'post': post, 'form': form, 'sent': sent}
 	return render(request, 'blog/post/share.html', context)
 
 
