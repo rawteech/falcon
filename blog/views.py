ee823384db230d38f63c183bd5a4c14ef8b97490
rawteech <rotichbil636@gmail.com>
2017-04-04 20:38:14 +0300

    Add ability to view similar posts

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.core.mail import send_mail
 from django.views.generic import ListView
+from django.db.models import Count
 
 from blog.models import Post, Comments
 from blog.forms import EmailPostForm, CommentForm
@@ -62,10 +63,15 @@ def post_detail(request, year, month, day, post):
 	else:
 		comment_form = CommentForm()
 
+	post_tags_ids = post.tags.values_list('id', flat=True)
+	similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+	similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
+
 	context = {
 		'post': post, 
 		'comments': comments, 
 		'comment_form': comment_form,
+		'similar_posts': similar_posts,
 	}
 	return render(request, 'blog/post/detail.html', context)
 
