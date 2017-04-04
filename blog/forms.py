26c565c9653ca59b63fc8bb9be744136f11b6aa2
rawteech <rotichbil636@gmail.com>
2017-04-04 03:51:10 +0300

    Form for sending mail

--- blog/forms.py
+++ blog/forms.py
@@ -5,5 +5,4 @@ class EmailPostForm(forms.Form):
 	name = forms.CharField(max_length=25)
 	email = forms.EmailField()
 	to = forms.EmailField()
-	comments = forms.CharField(required=False, widget=forms.TextArea)
-	
+	comments = forms.CharField(required=False, widget=forms.Textarea)
