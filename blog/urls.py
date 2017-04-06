f44d00aa840d240188ceafe7c6647f3ef0b2c2e7
rawteech <rotichbil636@gmail.com>
2017-04-06 07:15:17 +0300

    Include url for post feeds

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
+
 from blog import views
+from blog.feeds import LatestPostsFeed
 
 
 urlpatterns = [
@@ -9,4 +11,5 @@ urlpatterns = [
 	url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/'\
 		r'(?P<post>[-\w]+)/$', views.post_detail, name='post_detail'),
 	url(r'^(?P<post_id>\d+)/share/$', views.post_share, name='post_share'),
+	url(r'^feed/$', LatestPostsFeed(), name='post_feed'),
 ]
